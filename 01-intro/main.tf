cdac18e10a8c9bfa2c825ec0ca901457fdb7f850
Vamsi Krishna S <karthikvamsi21@gmail.com>
2025-02-26 09:54:25 +0530

    Roboshop code

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -6,7 +6,16 @@ output "x" {
   value = var.x
 }
 
+variable "y" {
+  default = [1,"abc",0.3,true]
+}
 output "x1" {
   value = "Value of x - ${var.x}"
 }
+
+output "y1" {
+  value = var.y[0]
+}
+
+
 
